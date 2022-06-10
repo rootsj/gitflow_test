d5f508e1d3a71bb89c0b39ffb1ea72a706272b50
이종화 <rootsj@hiconsy.com>
2022-06-10 10:44:26 +0900

    feature1 개발2차

--- src/main/java/com/example/gitflow_test/domains/board/controller/boardFeature1Controller.java
+++ src/main/java/com/example/gitflow_test/domains/board/controller/boardFeature1Controller.java
@@ -1,4 +1,11 @@
 package com.example.gitflow_test.domains.board.controller;
 
 public class boardFeature1Controller {
+
+    public void feature1() {
+        String test = "feature1 2차 반영";
+        String test2 = "feature1 2차 반영";
+        String test3 = "feature1 2차 반영";
+        String test4 = "feature1 2차 반영";
+    }
 }
