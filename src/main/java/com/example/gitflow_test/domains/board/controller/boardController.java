b5bd3e045ac7e2e40525b5248298aa31e161d703
이종화 <rootsj@hiconsy.com>
2022-06-10 10:40:37 +0900

    feature1 개발1차

--- src/main/java/com/example/gitflow_test/domains/board/controller/boardController.java
+++ src/main/java/com/example/gitflow_test/domains/board/controller/boardController.java
@@ -1,4 +1,11 @@
 package com.example.gitflow_test.domains.board.controller;
 
 public class boardController {
+
+    public void dev1commit () {
+        String test = "dev에서 feature1 작업한 코드입니다.";
+        String test2 = "dev에서 feature1 작업한 코드입니다.";
+        String test3 = "dev에서 feature1 작업한 코드입니다.";
+        String test4 = "dev에서 feature1 작업한 코드입니다.";
+    }
 }
