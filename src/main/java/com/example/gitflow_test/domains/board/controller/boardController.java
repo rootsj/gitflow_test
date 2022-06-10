8775c88347be83338143e2718fd1c07bdc491f51
이종화 <rootsj@hiconsy.com>
2022-06-10 12:29:01 +0900

    alpha 분기 버그2 수정

--- src/main/java/com/example/gitflow_test/domains/board/controller/boardController.java
+++ src/main/java/com/example/gitflow_test/domains/board/controller/boardController.java
@@ -6,6 +6,7 @@ public class boardController {
         String test = "dev에서 feature1 작업한 코드입니다.";
         String test2 = "dev에서 feature1 작업한 코드입니다.";
         String test3 = "dev에서 feature1 작업한 코드입니다.";
+        String test3AlphaBug222 = "dev에서 feature1 작업한 코드입니다. 알파에서 분기한 버그2입니다.";
         String test4 = "dev에서 feature1 작업한 코드입니다.";
     }
 }
