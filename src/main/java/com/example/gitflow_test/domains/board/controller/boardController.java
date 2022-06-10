a3cec46340f42c78a12af1275fb5436b396343f8
이종화 <rootsj@hiconsy.com>
2022-06-10 10:48:42 +0900

    dev-feature2 개발완료

--- src/main/java/com/example/gitflow_test/domains/board/controller/boardController.java
+++ src/main/java/com/example/gitflow_test/domains/board/controller/boardController.java
@@ -1,4 +1,10 @@
 package com.example.gitflow_test.domains.board.controller;
 
 public class boardController {
+
+    public void feature2Method() {
+        String test = "boardContoller에 중복 작업 입니다. feature2";
+        String test2 = "boardContoller에 중복 작업 입니다. feature2";
+        String test3 = "boardContoller에 중복 작업 입니다. feature2";
+    }
 }
---
+++ src/main/java/com/example/gitflow_test/domains/board/controller/feature2Controller.java
@@ -0,0 +1,10 @@
+package com.example.gitflow_test.domains.board.controller;
+
+public class feature2Controller {
+    public void feature2() {
+        String test = "feature2 개발영역입니다";
+        String test2 = "feature2 개발영역입니다";
+        String test3 = "feature2 개발영역입니다";
+        String test4 = "feature2 개발영역입니다";
+    }
+}
